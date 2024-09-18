656f6def465c406ffbc19f523960882b15bade25
Ahmed Wael <wael92318@gmail.com>
2024-09-19 01:01:33 +0300

    Update API

--- src/redux/AuthSlice.js
+++ src/redux/AuthSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/users";
+const API_URL = "https://my-json-server.typicode.com/ahmedwael29/server";
 export const registerUser = createAsyncThunk(
   "auth/register",
   async (userData, { rejectWithValue }) => {
