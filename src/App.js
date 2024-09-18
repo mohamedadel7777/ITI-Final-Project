8f4c13538e8c2586abf15285704f07911426deaf
Ahmed Wael <wael92318@gmail.com>
2024-09-18 22:26:46 +0300

    fix error at app.js

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
     <div className="font">
       {!isAdmin ? isLoggedIn ? <NavBarSignedIn /> : <NavBarLogin /> : null}
 
-      <HashRouter basename={`ITI-Final-Project`}>
+      <HashRouter>
         <Routes>
           <Route index element={<HomePage />} />
           <Route path="/Login" element={<Login />} />
